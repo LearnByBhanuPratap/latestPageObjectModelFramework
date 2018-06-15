f3ad7e3e7519c6a3b71da9a0d762dd0e068a4b14
Singh, Bhanu Pratap <BhanuPratap_Singh@intuit.com>
2018-06-16 02:20:26 +0530

    add

---
+++ src/main/java/com/uiFramework/companyName/bhanuProjectName/helper/listener/ExtentListener.java
@@ -0,0 +1,58 @@
+package com.uiFramework.companyName.bhanuProjectName.helper.listener;
+
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+import org.testng.Reporter;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.uiFramework.companyName.bhanuProjectName.utils.ExtentManager;
+
+public class ExtentListener implements ITestListener{
+
+	public static ExtentReports extent;
+	public static ExtentTest test;
+
+	public void onFinish(ITestContext arg0) {
+        extent.flush();
+        Reporter.log(arg0.getName()+" Test Finished..");
+	}
+
+	public void onStart(ITestContext arg0) {
+		extent = ExtentManager.getInstance();
+		test = extent.createTest(arg0.getCurrentXmlTest().getName());
+		Reporter.log(arg0.getCurrentXmlTest().getName()+" Class Started..");
+		
+	}
+
+	public void onTestFailedButWithinSuccessPercentage(ITestResult arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onTestFailure(ITestResult arg0) {
+		test.log(Status.FAIL, arg0.getThrowable());
+		Reporter.log(arg0.getMethod().getMethodName()+" Test Failed.."+arg0.getThrowable());
+		
+	}
+
+	public void onTestSkipped(ITestResult arg0) {
+		test.log(Status.SKIP, arg0.getThrowable());
+		Reporter.log(arg0.getMethod().getMethodName()+" Test Skipped.."+arg0.getThrowable());
+		
+	}
+
+	public void onTestStart(ITestResult arg0) {
+		test.log(Status.INFO, arg0.getName()+" started..");
+		Reporter.log(arg0.getMethod().getMethodName()+" Test Started..");
+		
+	}
+
+	public void onTestSuccess(ITestResult arg0) {
+		test.log(Status.INFO, arg0.getName()+" Passed..");
+		Reporter.log(arg0.getMethod().getMethodName()+" Test Passed..");
+	}
+
+}
