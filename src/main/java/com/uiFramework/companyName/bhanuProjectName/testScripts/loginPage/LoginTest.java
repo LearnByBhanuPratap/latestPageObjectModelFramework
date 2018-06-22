59e06366a5b971526ad74c6c85459db24fc2ea0f
Singh, Bhanu Pratap <BhanuPratap_Singh@intuit.com>
2018-06-22 23:05:06 +0530

    add

---
+++ src/main/java/com/uiFramework/companyName/bhanuProjectName/testScripts/loginPage/LoginTest.java
@@ -0,0 +1,27 @@
+package com.uiFramework.companyName.bhanuProjectName.testScripts.loginPage;
+
+import org.apache.log4j.Logger;
+import org.testng.annotations.Test;
+
+import com.uiFramework.companyName.bhanuProjectName.helper.assertion.AssertionHelper;
+import com.uiFramework.companyName.bhanuProjectName.helper.browserConfiguration.config.ObjectReader;
+import com.uiFramework.companyName.bhanuProjectName.helper.logger.LoggerHelper;
+import com.uiFramework.companyName.bhanuProjectName.pageObject.LoginPage;
+import com.uiFramework.companyName.bhanuProjectName.testbase.TestBase;
+/**
+ * 
+ * @author Bhanu Pratap
+ */
+public class LoginTest extends TestBase{
+	private final Logger log = LoggerHelper.getLogger(LoginTest.class);
+	
+	@Test
+	public void testLoginToApplication(){
+		getApplicationUrl(ObjectReader.reader.getUrl());
+		LoginPage loginPage = new LoginPage(driver);
+		loginPage.loginToApplication(ObjectReader.reader.getUserName(), ObjectReader.reader.getPassword());
+		boolean status = loginPage.verifySuccessLoginMsg();
+		AssertionHelper.updateTestStatus(status);
+	}
+
+}
