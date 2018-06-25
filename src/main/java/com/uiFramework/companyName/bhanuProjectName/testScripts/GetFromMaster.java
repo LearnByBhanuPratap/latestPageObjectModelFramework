6fd57a3b7c6aaa6c216059e9b225abe90809e7f0
Singh, Bhanu Pratap <BhanuPratap_Singh@intuit.com>
2018-06-25 23:22:07 +0530

    add

--- src/main/java/com/uiFramework/companyName/bhanuProjectName/testScripts/GetFromMaster.java
+++ src/main/java/com/uiFramework/companyName/bhanuProjectName/testScripts/GetFromMaster.java
@@ -2,4 +2,14 @@ package com.uiFramework.companyName.bhanuProjectName.testScripts;
 
 public class GetFromMaster {
 
+	
+	public void test(){
+		int a = 10;
+		int b = 30;
+		int c = a+b;
+		System.out.println(c);
+		for(int i =0; i<9; i++){
+			c = i;
+		}
+	}
 }
