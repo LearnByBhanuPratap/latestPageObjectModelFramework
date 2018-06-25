a27872ee2224146092da7b0abee20e4e079f046a
qbdt <info.bhanupratap@gmail.com>
2018-06-25 10:55:41 -0700

    adding latest file

--- src/main/java/com/uiFramework/companyName/bhanuProjectName/testScripts/GetFromMaster.java
+++ src/main/java/com/uiFramework/companyName/bhanuProjectName/testScripts/GetFromMaster.java
@@ -1,5 +1,15 @@
 package com.uiFramework.companyName.bhanuProjectName.testScripts;
 
 public class GetFromMaster {
+	
+	public void test1(){
+		int a = 10;
+		int b = 30;
+		int c = a+b;
+		System.out.println(c);
+		for(int i =0; i<9; i++){
+			c = i;
+		}
+	}
 
 }
